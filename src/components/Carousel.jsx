0c7d626abd45caecbeb1421c8a665010982f6ff6
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-03-04 21:12:49 +0100

    adding functions

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,21 @@
 import React, { useState } from 'react'
 import "./style.css"
+import { ImArrowLeft, ImArrowRight } from "react-icons/im";
 
 export const Carousel = ({data}) => {
     const [slide, setSlide] = useState(0)
     console.log(data);
 
+    
+    const prevSlide = () =>{};
+
+
+    const nextSlide = () =>{};
+
   return (
 
     <div className='carousel'>
+        <ImArrowLeft className='arrow arrow-left'/>
             {
                 data.map((item,idx) => {
                     return (
@@ -20,6 +28,14 @@ export const Carousel = ({data}) => {
                     )
                 })
             }
+        <ImArrowRight className='arrow arrow-right'/>
+        <span className='indicators'>
+            {
+                data.map((_,idx) =>{
+                    return <button onClick={() => setSlide(idx)} key={idx} className={slide === idx ? "indicator" : "indicator indicator-inactive"}></button>
+                })
+            }
+        </span>
     </div>
   )
 }
