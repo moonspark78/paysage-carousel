29058d0d28ac2b89dde519992fed4667f0d59893
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-03-04 18:14:24 +0100

    create the data

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Carousel } from "./components/Carousel";
+import slides from "./data/Carousel.json"
+
+function App() {
+  return (
+    <div className="App">
+      <Carousel data={slides.slides}/>
+    </div>
+  );
+}
+
+export default App;
