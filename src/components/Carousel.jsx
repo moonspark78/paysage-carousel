02bbe7f499b0c5c35a55f596d273fe942ab717b0
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-03-04 19:57:51 +0100

    create the data

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./style.css"
 
 export const Carousel = ({data}) => {
+    const [slide, setSlide] = useState(0)
+    console.log(data);
+
   return (
-    <div>Carousel</div>
+
+    <div className='carousel'>
+            {
+                data.map((item,idx) => {
+                    return (
+                        <img 
+                            className={slide === idx ? "slide" : "slide slide-hiden"}
+                            key={idx}
+                            alt={item.alt}
+                            src={item.src}
+                        />
+                    )
+                })
+            }
+    </div>
   )
 }
